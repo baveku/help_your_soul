b8636ba3f4641c453549d6505e90ade06392a8ea
bachvq <>
2020-11-15 17:03:08 +0700

    remove g.dart

--- submodules/corelib/lib/corelib.dart
+++ submodules/corelib/lib/corelib.dart
@@ -2,11 +2,13 @@ library corelib;
 
 import 'package:corelib/corelib.config.dart';
 import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+export 'package:corelib/type.dart';
 export 'package:corelib/service.dart';
 export 'package:corelib/core/films/repository.dart';
 export 'package:corelib/core/films/service.dart';
 export 'package:corelib/core/films/bloc/filmbloc.dart';
-import 'package:injectable/injectable.dart';
 
 // Define Public Function
 
